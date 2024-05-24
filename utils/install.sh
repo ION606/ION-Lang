2fc0229091af7a90cc170d24f1c37fe9c55f59e5
ION606 <ion606@protonmail.com>
2024-05-23 22:08:08 -0400

    update dependancies

--- utils/install.sh
+++ utils/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "THIS IS BROKEN"
+exit 1;
+
 # Clone the repository
-git clone "https://github.com/ION606/ION-Lang.git"
+git clone "https://github.com/ION606/ION-Lang.git" ~
 # Change to the cloned repository directory
 cd "ION-Lang" || exit
 
@@ -10,6 +13,8 @@ npm install
 
 # Run the relink script
 npm run relink
+
+# echo "alias ion='~/ION-Lang'" >> ./~bashrc
 
 echo "to run a file use \`ion run {PATH_TO_ION_FILE}\`"
 echo "to install a package use \`ion install {PATH_TO_ION_FILE}\` or \`ion i {PATH_TO_ION_FILE}\`"
